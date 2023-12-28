ec274a8269b60bbc85310cee93173848ada82915
Sophnel Merzier <johndoe@email.com>
2023-12-28 16:19:12 +0100

    Update RDS Postgres security group ingress ports

--- terraform/modules/security_groups/main.tf
+++ terraform/modules/security_groups/main.tf
@@ -45,8 +45,8 @@ resource "aws_security_group" "rds_postgres_sg" {
 
   ingress {
     description = "Source EKS Node ingress"
-    from_port   = 5432
-    to_port     = 5432
+    from_port   = -1
+    to_port     = -1
     protocol    = "tcp"
     security_groups = [ aws_security_group.eks_node_sg.id ]
   }
