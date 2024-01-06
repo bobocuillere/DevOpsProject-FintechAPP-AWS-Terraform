256f7d6df081b6e62361e2383dfb77a72234e393
Sophnel Merzier <johndoe@email.com>
2024-01-06 23:48:00 +0100

    Refactor wait-for-postgres.sh script to handle AWS RDS usage

--- src/wait-for-postgres.sh
+++ src/wait-for-postgres.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 
-# LOCAL ONLY : this script waits for the PostgreSQL database to become available before executing a command.
-# It takes the host as the first argument and the command to execute as the remaining arguments.
-
 set -e
 
+# Store the first argument in 'host' and then shift arguments to the left.
+# 'host' is expected to be the database host. The remaining arguments are the command to start the Flask app.
 host="$1"
 shift
 cmd="$@"
 
-# Environment variable to indicate if the app is using AWS RDS
+# Check if the app is using AWS RDS. The USE_AWS_RDS environment variable is set in your Kubernetes deployment.
 if [ "$USE_AWS_RDS" = "true" ]; then
+    # If using AWS RDS, output a message and skip the database waiting logic.
     >&2 echo "Using AWS RDS for PostgreSQL. Skipping wait-for-postgres script."
 else
+    # If not using AWS RDS, wait for the PostgreSQL database to become ready.
     until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$host" -U "$POSTGRES_USER" -c '\q'; do
         >&2 echo "Postgres is unavailable - sleeping"
         sleep 1
     done
+    # Output a message once the database is ready.
     >&2 echo "Postgres is up - executing command"
 fi
 
-# Execute the command (Flask application)
+# Execute the command to start the Flask application.
+# This line is crucial as it ensures that your Flask app starts regardless of the USE_AWS_RDS setting.
 exec $cmd
-
