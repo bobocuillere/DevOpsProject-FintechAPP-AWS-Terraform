97db6be700c9c2551abcf8d85b1fb57230461f53
Sophnel Merzier <johndoe@email.com>
2024-01-06 22:47:00 +0100

    Add AWS RDS support and improve PostgreSQL wait script

--- src/wait-for-postgres.sh
+++ src/wait-for-postgres.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
-# This script waits for the PostgreSQL database to become available before executing a command.
+# LOCAL ONLY : this script waits for the PostgreSQL database to become available before executing a command.
 # It takes the host as the first argument and the command to execute as the remaining arguments.
 
 set -e
 
-host="$1"
-shift
-cmd="$@"
+# Environment variable to indicate if the app is using AWS RDS
+if [ "$USE_AWS_RDS" = "true" ]; then
+    >&2 echo "Using AWS RDS for PostgreSQL. Skipping wait-for-postgres script."
+else
+    host="$1"
+    shift
+    cmd="$@"
 
-until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
-    >&2 echo "Postgres is unavailable - sleeping"
-    sleep 1
-done
+    until PGPASSWORD="$POSTGRES_PASSWORD" psql -h "$host" -U "$POSTGRES_USER" -c '\q'; do
+        >&2 echo "Postgres is unavailable - sleeping"
+        sleep 1
+    done
+fi
 
-
->&2 echo "Postgres is up - The web server is starting"
+>&2 echo "Postgres is up - executing command"
 exec $cmd
